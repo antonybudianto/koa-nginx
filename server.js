03ea34929ae186021231fa6fdd59a01ab3a97a9a
Antony <antony.budianto@tokopedia.com>
2021-03-17 11:38:38 +0700

    handle public

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ const os = require("os");
 const app = new Koa();
 
 app.use(async (ctx) => {
-  ctx.body = "hello! " + ctx.request.querystring;
+  ctx.response.type = "text/html";
+  ctx.body = `
+    <!DOCTYPE html>
+    <html>
+      <body>
+        <h1>Hello Koa</h1>
+      </body>
+    </html>
+  `;
 });
 
 module.exports = app;
